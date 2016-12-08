9221c0184501e66948721a88adf2b5caddd9d915
Jonathan Helmus <jjhelmus@gmail.com>
2016-12-08 19:00:52 +0000

    Add ncurses recipe
    Adapted from conda-forge feedstock

---
+++ recipes/ncurses/build.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+
+for USE_WIDEC in false true;
+do
+    WIDEC_OPT=""
+    if [ "${USE_WIDEC}" = true ];
+    then
+        WIDEC_OPT="--enable-widec"
+    fi
+
+    sh ./configure \
+	    --prefix=$PREFIX \
+	    --without-debug \
+	    --without-ada \
+	    --without-manpages \
+	    --with-shared \
+	    --disable-overwrite \
+	    --enable-symlinks \
+	    --enable-termcap \
+	    --with-termlib \
+	    $WIDEC_OPT
+    make -j4
+    make install
+    make clean
+    make distclean
+
+    # Provide headers in `$PREFIX/include` and
+    # symlink them in `$PREFIX/include/ncurses`
+    # and in `$PREFIX/include/ncursesw`.
+    HEADERS_DIR="${PREFIX}/include/ncurses"
+    if [ "${USE_WIDEC}" = true ];
+    then
+        HEADERS_DIR="${PREFIX}/include/ncursesw"
+    fi
+    for HEADER in $(ls $HEADERS_DIR);
+    do
+        mv "${HEADERS_DIR}/${HEADER}" "${PREFIX}/include/${HEADER}"
+        ln -s "${PREFIX}/include/${HEADER}" "${HEADERS_DIR}/${HEADER}"
+    done
+done
