988ebcaf9b34cb65672932823d9d1d08d4511061
Jonathan Helmus <jjhelmus@gmail.com>
2016-12-08 01:48:15 +0000

    Add conda recipe
    Adapted from the conda-forge feedstock

---
+++ recipes/conda/build.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Remove symlinks
+unlink $PREFIX/bin/conda
+unlink $PREFIX/bin/activate
+unlink $PREFIX/bin/deactivate
+
+# Prep conda install
+export CONDA_DEFAULT_ENV=''
+echo "${PKG_VERSION}" > conda/.version
+
+# Install the Python code
+$PYTHON setup.py install
+
+# Install the fish activation script.
+mkdir -p $PREFIX/etc/fish/conf.d/
+cp $SRC_DIR/shell/conda.fish $PREFIX/etc/fish/conf.d/
+
+# Link everything in `exec`.
+mkdir -p $PREFIX/exec
+ln -s $PREFIX/bin/activate $PREFIX/exec/activate
+ln -s $PREFIX/bin/deactivate $PREFIX/exec/deactivate
+ln -s $PREFIX/bin/conda $PREFIX/exec/conda
