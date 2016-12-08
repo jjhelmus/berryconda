d4d8885eaab84a2844c605f205d652cd1eacf37b
Jonathan Helmus <jjhelmus@gmail.com>
2016-12-08 22:39:59 +0000

    Add python 3.5 recipe
    Adapted from the conda-forge feedstock

---
+++ recipes/python-3.5/build.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+python ${RECIPE_DIR}/brand_python.py
+
+./configure --enable-shared --enable-ipv6 --with-ensurepip=no \
+    --prefix=$PREFIX \
+    --with-tcltk-includes="-I$PREFIX/include" \
+    --with-tcltk-libs="-L$PREFIX/lib -ltcl8.6 -ltk8.6" \
+    CPPFLAGS="-I$PREFIX/include" \
+    LDFLAGS="-L$PREFIX/lib -Wl,-rpath=$PREFIX/lib,--no-as-needed"
+
+make -j4
+make install
+ln -s $PREFIX/bin/python3.5 $PREFIX/bin/python
+ln -s $PREFIX/bin/pydoc3.5 $PREFIX/bin/pydoc
