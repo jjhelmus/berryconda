8bfe41e96d80c564e5c408360c196fa08b8c9ace
Jonathan Helmus <jjhelmus@gmail.com>
2016-12-09 02:05:24 +0000

    Add ptyprocess recipe

---
+++ recipes/ptyprocess/setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='ptyprocess',
+      version='0.5.1',
+      description='Run a subprocess in a pseudo terminal',
+      author='Thomas Kluyver',
+      author_email='thomas@kluyver.me.uk',
+      url='https://github.com/pexpect/ptyprocess',
+      packages=['ptyprocess']
+     )
