3c703c1a805ecd7a70e5257d8971f420719154ea
Jonathan Helmus <jjhelmus@gmail.com>
2016-12-11 01:23:22 +0000

    Add numpy and openblas recipe
    Adapted from the conda-forge feedstocks and anaconda-recipes

---
+++ recipes/openblas/build.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+case `uname -m` in
+  'armv7l' )
+    TARGET=ARMV7
+    ;;
+  * )
+    TARGET=NEHALEM
+    ;;
+esac
+
+OPTS="USE_THREAD=1 NO_LAPACK=0 TARGET=$TARGET NO_STATIC=1"
+
+make FC=gfortran $OPTS -j$CPU_COUNT
+OPENBLAS_NUM_THREADS=$CPU_COUNT make test
+make PREFIX=$PREFIX $OPTS install
