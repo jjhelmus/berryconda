d2ca1953b409f09a198d49a9380f496823a6ee9c
Jonathan Helmus <jjhelmus@gmail.com>
2016-12-15 20:46:55 -0600

    Add libgfortran recipe
    Adapted from anaconda-recipes

---
+++ recipes/libgfortran/build.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+LIB=$PREFIX/lib
+mkdir $LIB
+cd $LIB
+
+if [ `uname -m` == 'armv7l' ]; then
+    cp /usr/lib/arm-linux-gnueabihf/libgfortran.so.3.0.0 .
+    ln -s libgfortran.so.3.0.0 libgfortran.so.3
+fi
