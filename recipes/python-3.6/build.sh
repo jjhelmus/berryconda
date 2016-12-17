7c695186eb47ed7beaed82b0df3f29b1ee949205
Jonathan Helmus <jjhelmus@gmail.com>
2016-12-17 15:39:11 +0000

    Add Python 3.6.0 recipe

---
+++ recipes/python-3.6/build.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+python ${RECIPE_DIR}/brand_python.py
+
+# Remove test data and ensurepip stubs to save space
+rm -rf Lib/test Lib/*/test
+rm -rf Lib/ensurepip
+
+./configure --enable-shared --enable-ipv6 --with-ensurepip=no \
+    --prefix=$PREFIX \
+    --with-tcltk-includes="-I$PREFIX/include" \
+    --with-tcltk-libs="-L$PREFIX/lib -ltcl8.6 -ltk8.6" \
+    CPPFLAGS="-I$PREFIX/include" \
+    LDFLAGS="-L$PREFIX/lib -Wl,-rpath=$PREFIX/lib,--no-as-needed"
+
+make -j${CPU_COUNT}
+make install
+ln -s $PREFIX/bin/python3.6 $PREFIX/bin/python
+ln -s $PREFIX/bin/pydoc3.6 $PREFIX/bin/pydoc
