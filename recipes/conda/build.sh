3d4fb4c6becb2160715abda4e3e46fb206dea3f2
Jonathan Helmus <jjhelmus@gmail.com>
2017-06-17 06:50:10 -0500

    Update conda recipe to version 4.3.22

--- recipes/conda/build.sh
+++ recipes/conda/build.sh
@@ -15,9 +15,3 @@ $PYTHON conda.recipe/setup.py install
 # Install the fish activation script.
 mkdir -p $PREFIX/etc/fish/conf.d/
 cp $SRC_DIR/shell/conda.fish $PREFIX/etc/fish/conf.d/
-
-# Link everything in `exec`.
-mkdir -p $PREFIX/exec
-ln -s $PREFIX/bin/activate $PREFIX/exec/activate
-ln -s $PREFIX/bin/deactivate $PREFIX/exec/deactivate
-ln -s $PREFIX/bin/conda $PREFIX/exec/conda
