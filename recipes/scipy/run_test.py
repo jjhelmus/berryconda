4b7d92e864c62bf1d036601ce620f06ccb7ea6bf
Jonathan Helmus <jjhelmus@gmail.com>
2017-10-28 15:17:46 -0500

    update scipy to 1.0

--- recipes/scipy/run_test.py
+++ recipes/scipy/run_test.py
@@ -24,7 +24,6 @@ import scipy.interpolate.interpnd
 import scipy.io.matlab.mio5_utils
 import scipy.io.matlab.mio_utils
 import scipy.io.matlab.streams
-import scipy.linalg._calc_lwork
 import scipy.linalg._decomp_update
 import scipy.linalg._fblas
 import scipy.linalg._flapack
@@ -74,4 +73,4 @@ import scipy.stats
 import scipy.special
 
 
-sys.exit(not scipy.test().wasSuccessful())
+sys.exit(scipy.test())
