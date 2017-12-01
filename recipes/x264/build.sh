05deff8a28ed0e97efff5c0d1214c9b456b3c80a
Jonathan Helmus <jjhelmus@gmail.com>
2017-12-01 09:34:49 -0600

    modify x264 recipe for use in berryconda

--- recipes/x264/build.sh
+++ recipes/x264/build.sh
@@ -2,15 +2,6 @@
 
 mkdir -vp ${PREFIX}/bin
 
-CFLAGS="-Wall -g -m64 -pipe -O2 -fPIC"
-if [[ $ARCH = 64 ]]; then
-    CFLAGS="${CFLAGS} -march=x86-64"
-else
-    CFLAGS="${CFLAGS} -march=i386"
-fi
-export CFLAGS
-export CXXLAGS="${CFLAGS}"
-
 chmod +x configure
 ./configure \
         --enable-pic \
