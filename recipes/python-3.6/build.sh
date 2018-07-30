2228f6c32b5c218d19905b841cc573a3639f32df
Gaiar Baimuratov <flibustier@gmail.com>
2018-07-30 13:39:15 +0200

    Python 3.6.6 (#32)
    * bump python to 3.6.6. enable optimizations

--- recipes/python-3.6/build.sh
+++ recipes/python-3.6/build.sh
@@ -7,12 +7,12 @@ rm -rf Lib/test Lib/*/test
 rm -rf Lib/ensurepip
 
 ./configure --enable-shared --enable-ipv6 --with-ensurepip=no \
-    --prefix=$PREFIX \
-    --with-tcltk-includes="-I$PREFIX/include" \
-    --with-tcltk-libs="-L$PREFIX/lib -ltcl8.6 -ltk8.6" \
-    CPPFLAGS="-I$PREFIX/include" \
-    LDFLAGS="-L$PREFIX/lib -Wl,-rpath=$PREFIX/lib,--no-as-needed"
-
+	--enable-optimizations \
+	--prefix=$PREFIX \
+	--with-tcltk-includes="-I$PREFIX/include" \
+	--with-tcltk-libs="-L$PREFIX/lib -ltcl8.6 -ltk8.6" \
+	CPPFLAGS="-I$PREFIX/include" \
+	LDFLAGS="-L$PREFIX/lib -Wl,-rpath=$PREFIX/lib,--no-as-needed"
 make -j${CPU_COUNT}
 make install
 ln -s $PREFIX/bin/python3.6 $PREFIX/bin/python
