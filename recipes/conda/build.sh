74c7b3584a6758f8cfff6621fdbb0aa109222af0
Jonathan Helmus <jjhelmus@gmail.com>
2018-07-31 09:51:59 -0500

    conda: update to version 4.5.9

--- recipes/conda/build.sh
+++ recipes/conda/build.sh
@@ -1,17 +1,7 @@
 #!/bin/bash
 
-# Remove symlinks if they exist
-unlink $PREFIX/bin/conda || true
-unlink $PREFIX/bin/activate || true
-unlink $PREFIX/bin/deactivate || true
-
 # Prep conda install
-export CONDA_DEFAULT_ENV=''
 echo "${PKG_VERSION}" > conda/.version
 
-# Install the Python code
-$PYTHON conda.recipe/setup.py install
-
-# Install the fish activation script.
-mkdir -p $PREFIX/etc/fish/conf.d/
-cp $SRC_DIR/shell/conda.fish $PREFIX/etc/fish/conf.d/
+# Install conda using bash function
+. utils/functions.sh && install_conda_full
