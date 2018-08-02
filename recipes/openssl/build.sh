2a95aa9da6dc8765f6079556b44144a3f6566285
Jonathan Helmus <jjhelmus@gmail.com>
2018-08-02 09:23:06 -0500

    openssl: update to 1.0.2o

--- recipes/openssl/build.sh
+++ recipes/openssl/build.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-./config shared --prefix=$PREFIX
+# Configure OpenSSL manually to prevent it from building for armv7-a
+# "./config -t" can be used to see the automatically determine configuration
+./Configure linux-armv4 shared --prefix=${PREFIX} -march=armv6 -mfpu=vfp -mfloat-abi=hard -Wa,--noexecstack
 make -j$CPU_COUNT
 make test
 make install
