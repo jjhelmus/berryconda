cb33003660221e5a756eb75d10f709b2509457b6
Jonathan Helmus <jjhelmus@gmail.com>
2018-09-03 19:27:57 +0000

    adjust sys_compilers recipe
    set additional flags
    set arch specific arguments based target platform
    rename from activate_system_compilers to sys_compilers

--- nonstandard_recipes/system_compilers/deactivate.sh
+++ nonstandard_recipes/system_compilers/deactivate.sh
@@ -2,6 +2,11 @@
 
 unset CC
 unset CXX
+unset FC
+unset F77
+unset F90
+
+unset CPPFLAGS
 unset CFLAGS
 unset CXXFLAGS
 unset LDFLAGS
--- nonstandard_recipes/system_compilers/install-system_compilers.sh
+++ nonstandard_recipes/system_compilers/install-system_compilers.sh
@@ -2,8 +2,9 @@
 
 # Copy the [de]activate scripts to $PREFIX/etc/conda/[de]activate.d.
 # This will allow them to be run on environment activation.
-for CHANGE in "activate" "deactivate"
-do
-    mkdir -p "${PREFIX}/etc/conda/${CHANGE}.d"
-    cp "${RECIPE_DIR}/${CHANGE}.sh" "${PREFIX}/etc/conda/${CHANGE}.d/${PKG_NAME}_${CHANGE}.sh"
-done
+
+mkdir -p "${PREFIX}/etc/conda/activate.d"
+cp "${RECIPE_DIR}/activate-${target_platform}.sh" "${PREFIX}/etc/conda/activate.d/${PKG_NAME}_activate.sh"
+
+mkdir -p "${PREFIX}/etc/conda/deactivate.d"
+cp "${RECIPE_DIR}/deactivate.sh" "${PREFIX}/etc/conda/deactivate.d/${PKG_NAME}_deactivate.sh"
