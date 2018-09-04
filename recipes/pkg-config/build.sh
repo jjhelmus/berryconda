843ef953f75381caec0285382decca4240120afb
Jonathan Helmus <jjhelmus@gmail.com>
2018-09-04 12:12:03 +0000

    pkg-config: update to 0.29.2

--- recipes/pkg-config/build.sh
+++ recipes/pkg-config/build.sh
@@ -1,10 +1,17 @@
 #!/bin/sh
 
-export CFLAGS="-I$PREFIX/include"
-export LDFLAGS="-L$PREFIX/lib"
-export PYTHON="/usr/bin/python"
-./configure --prefix=$PREFIX   \
+mkdir -p ${PREFIX}/include
+
+./configure --prefix=${PREFIX}    \
+            --host=${HOST}        \
             --with-internal-glib
 
-make
+make -j${CPU_COUNT} ${VERBOSE_AT}
 make install
+
+# conda customization for CDT packages, emitted for other OSes too incase of cross-compilation
+mv ${PREFIX}/bin/pkg-config ${PREFIX}/bin/pkg-config.bin
+cp "${RECIPE_DIR}"/pkg-config ${PREFIX}/bin
+rm -f ${PREFIX}/bin/${HOST}-pkg-config
+ln -s ${PREFIX}/bin/pkg-config ${PREFIX}/bin/${HOST}-pkg-config
+chmod +x ${PREFIX}/bin/pkg-config ${PREFIX}/bin/${HOST}-pkg-config
