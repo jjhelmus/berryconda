3d2dbd37a36fe911e98d7c8192281200f02a8922
Jonathan Helmus <jjhelmus@gmail.com>
2018-09-04 13:15:37 +0000

    readline: update to 7.0

--- recipes/readline/build.sh
+++ recipes/readline/build.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
-./configure --prefix=$PREFIX || { cat config.log; exit 1; }
-make -j${CPU_COUNT}
+./configure --prefix=${PREFIX}  \
+            --build=${BUILD}    \
+            --host=${HOST}      \
+            || { cat config.log; exit 1; }
+make SHLIB_LIBS="$(pkg-config --libs ncurses)" -j${CPU_COUNT} ${VERBOSE_AT}
 make install
