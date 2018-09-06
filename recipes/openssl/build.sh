d70373010a7d65c59019b89f3615de805c7d49ff
Jonathan Helmus <jjhelmus@gmail.com>
2018-09-06 10:45:23 +0000

    openssl: update to 1.0.2p

--- recipes/openssl/build.sh
+++ recipes/openssl/build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+PERL=${BUILD_PREFIX}/bin/perl
 # Configure OpenSSL manually to prevent it from building for armv7-a
 # "./config -t" can be used to see the automatically determine configuration
 ./Configure linux-armv4 shared --prefix=${PREFIX} -march=armv6 -mfpu=vfp -mfloat-abi=hard -Wa,--noexecstack
