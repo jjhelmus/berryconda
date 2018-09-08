9601d0d1a457df6c174214343c32ef49414f9db1
Jonathan Helmus <jjhelmus@gmail.com>
2018-09-08 22:37:28 +0000

    pillow 5.2.0

--- recipes/pillow/build.sh
+++ recipes/pillow/build.sh
@@ -8,4 +8,4 @@ export TIFF_ROOT=$PREFIX
 export FREETYPE_ROOT=$PREFIX
 # export LCMS_ROOT=None
 
-$PYTHON setup.py install --single-version-externally-managed --record=record.txt
+$PYTHON -m pip install . --no-deps --ignore-installed --no-cache-dir -vvv
