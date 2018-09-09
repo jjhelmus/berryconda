630d31adb6ead2fffb51d09a1a6932507ddaa45e
Jonathan Helmus <jjhelmus@gmail.com>
2018-09-09 11:50:57 +0000

    patchelf: sync with conda-forge

--- recipes/patchelf/build.sh
+++ recipes/patchelf/build.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-./configure --prefix=$PREFIX
-make
+./configure --prefix=${PREFIX}  \
+            --host=${HOST}      \
+            --build=${BUILD}
+make -j${CPU_COUNT} ${VERBOSE_AT}
 make tests
 make install
