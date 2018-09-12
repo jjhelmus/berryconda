26c6924ab9102351245b2ae90592c3d82c9fa1cd
Jonathan Helmus <jjhelmus@gmail.com>
2018-09-12 17:04:51 +0000

    pycosat: sync with conda-forge

--- recipes/pycosat/run_test.py
+++ recipes/pycosat/run_test.py
@@ -1,7 +1,13 @@
 #!/usr/bin/env python
+import pycosat
+import test_pycosat
 
-import os
-import subprocess
-import sys
+assert test_pycosat.run().wasSuccessful()
 
-subprocess.check_call([sys.executable, "test_pycosat.py"])
+assert pycosat.__version__ == '0.6.3'
+
+assert test_pycosat.process_cnf_file('qg3-08.cnf') == 18
+assert test_pycosat.process_cnf_file('uf20-098.cnf') == 5
+
+import sudoku
+sudoku.test()
