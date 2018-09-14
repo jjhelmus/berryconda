09626ad40126b554d3d44d9a70b2a68e6c2aa2e3
Jonathan Helmus <jjhelmus@gmail.com>
2018-09-14 13:24:57 +0000

    libxml2 2.9.8

--- recipes/libxml2/build.sh
+++ recipes/libxml2/build.sh
@@ -1,13 +1,24 @@
 #!/bin/bash
 
-export LIBRARY_SEARCH_VAR=LD_LIBRARY_PATH
-
 ./autogen.sh
 
 ./configure --prefix="${PREFIX}" \
+            --build=$BUILD \
+            --host=$HOST \
+            --with-iconv="${PREFIX}" \
             --with-zlib="${PREFIX}" \
+            --with-icu \
             --with-lzma="${PREFIX}" \
             --without-python
-make
-eval ${LIBRARY_SEARCH_VAR}=$PREFIX/lib make check
+make -j${CPU_COUNT} ${VERBOSE_AT}
+# Sorry:
+# ## Error cases regression tests
+# file result/errors/759573.xml.err is 1983 bytes, result is 1557 bytes
+# Error for ./test/errors/759573.xml failed
+# if [[ ${target_platform} != osx-64 ]]; then
+#   make check $VERBOSE_AT}
+# fi
 make install
+
+# remove libtool files
+find $PREFIX -name '*.la' -delete
